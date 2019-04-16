487ed868449867f5e362f9a73360c609bff2183d
Jeff Moeller <jrm252@yahoo.com>
2019-04-15 23:36:12 -0400

    test email and username

--- c/euhler.c
+++ c/euhler.c
@@ -359,15 +359,15 @@ void problem12()
 
 int main()
 {
-    problem1();
-    problem2();
-    problem3();
-    problem4();
-    problem5();
-    problem6();
-    problem7();
-    problem8();
-    problem9();
+    //problem1();
+    //problem2();
+    //problem3();
+    //problem4();
+    //problem5();
+    //problem6();
+    //problem7();
+    //problem8();
+    //problem9();
     //problem10();    //slow
     problem11();
     problem12();
